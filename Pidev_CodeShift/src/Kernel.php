6a679d7b3e6b52d730228e345b901d6f7d2ac87b
SyrineJazi <syrine.jazi@esprit.tn>
2024-02-28 19:35:30 +0100

    Generer un PDF
    Generer un PDF

--- Pidev_CodeShift/src/Kernel.php
+++ Pidev_CodeShift/src/Kernel.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Knp\Bundle\SnappyBundle\KnpSnappyBundle;
 
 class Kernel extends BaseKernel
 {
